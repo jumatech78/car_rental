b92842f86d1604c62b6909b03cd1c24dae97874e
jumaTech <86480489+jumatech78@users.noreply.github.com>
2022-07-30 13:51:12 +0300

    updates

--- litrides/screens/CarDetails/index.js
+++ litrides/screens/CarDetails/index.js
@@ -16,6 +16,12 @@ import AntDesign from "react-native-vector-icons/AntDesign";
 import Entypo from "react-native-vector-icons/Entypo";
 
 import q7 from "../../assets/q7.png";
+import topSpeed from "../../assets/top-speed.png";
+import wifi from "../../assets/wifi.png";
+import seat from "../../assets/seat.png";
+import sensor from "../../assets/sensors.png";
+import bluetooth from "../../assets/bluetooth.png";
+import cardoor from "../../assets/cardoor.png"
 
 function CarDetails() {
   const navigation = useNavigation();
@@ -59,7 +65,88 @@ function CarDetails() {
             <Text style={{ color: "white", fontSize: 18 }}>4.8</Text>
           </View>
         </View>
-        <Text>second</Text>
+        <Text
+          style={{
+            color: "white",
+            fontSize: 20,
+            paddingTop: 23,
+            paddingLeft: 20,
+          }}
+        >
+          Features
+        </Text>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            justifyContent: "space-evenly",
+            paddingTop: 15,
+          }}
+        >
+          <View style={styles.crd}>
+            <Image source={topSpeed} style={{ width: 30, height: 30 }} />
+            <Text
+              style={{ color: "#B9BABC", fontWeight: "bold", paddingTop: 5 }}
+            >
+              TOP SPEED
+            </Text>
+            <Text style={{ color: "white", paddingTop: 5 }}>280 km/hr</Text>
+          </View>
+          <View style={styles.crd}>
+            <Image source={wifi} style={{ width: 30, height: 35 }} />
+            <Text
+              style={{ color: "#B9BABC", fontWeight: "bold", paddingTop: 5 }}
+            >
+              WIFI
+            </Text>
+            <Text style={{ color: "white", paddingTop: 5 }}>Available</Text>
+          </View>
+          <View style={styles.crd}>
+            <Image source={seat} style={{ width: 30, height: 25 }} />
+            <Text
+              style={{ color: "#B9BABC", fontWeight: "bold", paddingTop: 5 }}
+            >
+              SEATS
+            </Text>
+            <Text style={{ color: "white", paddingTop: 5 }}>4</Text>
+          </View>
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            justifyContent: "space-evenly",
+            paddingTop: 15,
+          }}
+        >
+          <View style={styles.crd}>
+            <Image source={sensor} style={{ width: 30, height: 30 }} />
+            <Text
+              style={{ color: "#B9BABC", fontWeight: "bold", paddingTop: 5 }}
+            >
+              SENSOR
+            </Text>
+            <Text style={{ color: "white", paddingTop: 5 }}>Available</Text>
+          </View>
+          <View style={styles.crd}>
+            <Image source={bluetooth} style={{ width: 30, height: 30 }} />
+            <Text
+              style={{ color: "#B9BABC", fontWeight: "bold", paddingTop: 5 }}
+            >
+              BLUETOOTH
+            </Text>
+            <Text style={{ color: "white", paddingTop: 5 }}>4.0</Text>
+          </View>
+          <View style={styles.crd}>
+            <Image source={cardoor} style={{ width: 30, height: 22 }} />
+            <Text
+              style={{ color: "#B9BABC", fontWeight: "bold", paddingTop: 5 }}
+            >
+              DOORS
+            </Text>
+            <Text style={{ color: "white", paddingTop: 5 }}>4</Text>
+          </View>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -91,5 +178,14 @@ const styles = StyleSheet.create({
     paddingRight: 20,
     flexDirection: "row",
     justifyContent: "space-between",
+  },
+  crd: {
+    backgroundColor: "#171A1F",
+    height: 100,
+    width: 100,
+    borderRadius: 10,
+    alignItems: "center",
+    // paddingTop:15,
+    justifyContent: "center",
   },
 });
